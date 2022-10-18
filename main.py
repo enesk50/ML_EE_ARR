fc0dea746eec69e5be667295669975f2ef1f3d89
Enes Kinaci <e.kinaci@student.tudelft.nl>
2022-10-18 15:24:06 +0200

    First push

--- main.py
+++ main.py
@@ -14,10 +14,9 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    # Insert own directory
     df = pd.read_csv(r'C:\Users\NS_tu\Documents\School\Q1\EE4C12 Machine Learning for EE\Project\CE_ARR\Project_Data_EE4C12_CE_ARR\ECG_data\Beats_nsvfq_48p.csv', header=None)
-    for i in range(0,  10):
-        df.iloc[i].plot(label = ("plot"+str(i)))
-    plt.show()
+
 
 
 
