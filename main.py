f50fb6c5d6302bfa1f53547babe5bb1d9a97a11a
Enes Kinaci <e.kinaci@student.tudelft.nl>
2022-10-18 15:15:07 +0200

    Push

---
+++ main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from ecgdetectors import Detectors
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    df = pd.read_csv(r'C:\Users\NS_tu\Documents\School\Q1\EE4C12 Machine Learning for EE\Project\CE_ARR\Project_Data_EE4C12_CE_ARR\ECG_data\Beats_nsvfq_48p.csv', header=None)
+    for i in range(0,  10):
+        df.iloc[i].plot(label = ("plot"+str(i)))
+    plt.show()
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
