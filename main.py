c5ae29d87143bd4f8404683cb33f341e2bddbe42
jprozestraten <38658925+jprozestraten@users.noreply.github.com>
2022-10-20 15:55:07 +0200

    First working model

--- main.py
+++ main.py
@@ -1,8 +1,76 @@
-import numpy as np
+import os
+import time
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
+import keras.api._v2.keras as keras
+from keras import layers, models
 from ecgdetectors import Detectors
 
 if __name__ == '__main__':
-    # Insert own directory
-    df = pd.read_csv(r'C:\Users\NS_tu\Documents\School\Q1\EE4C12 Machine Learning for EE\Project\CE_ARR\Project_Data_EE4C12_CE_ARR\ECG_data\Beats_nsvfq_48p.csv', header=None)
+    # TIMING
+    time_start = time.time()
+
+    # Data loading
+    file_path = os.path.join(".", "ECG_data", "train_data.csv")
+    df = pd.read_csv(file_path, header=None)
+    df_ECG_train = df.iloc[:, 0:-1]
+    ds_lab_train = df.iloc[:, -1]
+
+    file_path = os.path.join(".", "ECG_data", "test_data.csv")
+    df = pd.read_csv(file_path, header=None)
+    df_ECG_test = df.iloc[:, 0:-1]
+    ds_lab_test = df.iloc[:, -1]
+
+    file_path = os.path.join(".", "ECG_data", "val_data.csv")
+    df = pd.read_csv(file_path, header=None)
+    df_ECG_val = df.iloc[:, 0:-1]
+    ds_lab_val = df.iloc[:, -1]
+
+    # TIMING
+    time_stop = time.time()
+    time_loading = time_stop - time_start
+
+    # Model setup
+    n_time_steps = 250
+    input_shape = (n_time_steps,1)
+
+    model = models.Sequential()
+
+    model.add(layers.Conv1D(filters=125, kernel_size=3, activation='relu', input_shape=input_shape))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(units=15, activation='softmax'))
+
+    model.summary()
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+                  metrics=['accuracy'])
+    
+    # Fit the model
+    history = model.fit(df_ECG_train, ds_lab_train, epochs=10, 
+                    validation_data=(df_ECG_val, ds_lab_val))
+
+    # TIMING #
+    time_stop = time.time()
+    time_tensor = time_stop - time_start
+
+    # Check accuracy with test data
+    # lab_predict = model.predict(df_ECG_test)
+
+    
+    # TIMING #
+    print("_" * 80)
+    print()
+    print("Loading time: ", str(time_loading))
+    print("Tensor flow time: ", str(time_tensor))
+
+    # Epoch plot
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.ylim([0.5, 1])
+    plt.legend(loc='lower right')
+    plt.show()
